2b36aa19b9576930b36b3e70d4a7277cc69d34b9
Robby <nxgrenz@gmail.com>
2024-01-06 21:37:37 -0600

    add basic record classes

--- src/main/java/com/robbyrenz/Main.java
+++ src/main/java/com/robbyrenz/Main.java
@@ -1,7 +1,28 @@
 package com.robbyrenz;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.SlackEmoji;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Faker faker = new Faker();
+        SlackEmoji slackEmoji = faker.slackEmoji();
+        System.out.println(slackEmoji.celebration());
     }
+
+    private record Person(FullName fullName,
+                          PersonalInfo personalInfo,
+                          FunFacts funFacts) {}
+
+    private record FullName(String firstName,
+                            String lastName) {}
+    private record PersonalInfo(int age,
+                                String streetAddress,
+                                String graduatedFromUniversity,
+                                String job) {}
+
+    private record FunFacts(String pet,
+                            String favoriteColor,
+                            String favoriteBook,
+                            String favoritePokemon) {}
 }
